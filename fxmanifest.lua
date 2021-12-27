515642b1a62dea70abc71c1767dc991cda862633
SeanMANTLE <96751957+SeanMANTLE@users.noreply.github.com>
2021-12-27 22:55:58 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'bodacious'
+game 'gta5'
+server_only 'yes'
+
+description 'Mantle Network Project'
+
+server_script 'server.lua'
---
+++ server.lua
@@ -0,0 +1,46 @@
+notWhitelistedMessage = "You just need to join the discord to connect to the Mantle Development Project" 
+
+whitelistRoles = { 
+    "Your Role ID",
+}
+
+AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
+    local src = source
+    local passAuth = false
+    deferrals.defer()
+    deferrals.update("Checking Permissions...")
+
+    for k, v in ipairs(GetPlayerIdentifiers(src)) do
+        if string.sub(v, 1, string.len("discord:")) == "discord:" then
+            identifierDiscord = v
+        end
+    end
+
+    if identifierDiscord then
+        usersRoles = exports.discord_perms:GetRoles(src)
+        local function has_value(table, val)
+            if table then
+                for index, value in ipairs(table) do
+                    if value == val then
+                        return true
+                    end
+                end
+            end
+            return false
+        end
+        for index, valueReq in ipairs(whitelistRoles) do 
+            if has_value(usersRoles, valueReq) then
+                passAuth = true
+            end
+            if next(whitelistRoles,index) == nil then
+                if passAuth == true then
+                    deferrals.done()
+                else
+                    deferrals.done(notWhitelistedMessage)
+                end
+            end
+        end
+    else
+        deferrals.done("Discord is not detected, check that it is installed and open.")
+    end
+end)
